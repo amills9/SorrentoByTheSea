407af695efae994751106d9045c4bc21fd03b1a5
amills9 <89893713+amills9@users.noreply.github.com>
2021-10-11 12:39:50 +1100

    Commit Next change

--- javascript/selectdates.js
+++ javascript/selectdates.js
@@ -1,2 +1,20 @@
 "use strict";
 
+let firstDay, lastDay;
+let days = document.getElementsByClassName("days")[0].children;
+
+for (let i = 0; i < days.length; i++) {
+    days[i].addEventListener("click", () => {
+      let day = days[i].innerHTML;
+
+      if (firstDay == undefined || lastDay == undefined) {
+        if (firstDay == undefined) {
+          firstDay = day;
+          console.log(firstDay + " is your first day")
+        } else {
+          lastDay = day;
+          console.log(lastDay + " is your second day")
+        }
+      }
+    })
+}
